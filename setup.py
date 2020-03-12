017cb131cc41a36bc2876b44fba6c45d6f5695a2
Chris Harding <c.p.harding@gmail.com>
2020-03-12 21:30:26 +0000

    setup mods

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='log_ext',
     version='1.0.2',
@@ -9,6 +12,7 @@ setuptools.setup(
     author='CpHarding',
     author_email='',
     description='An extension for standard python logging',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires='>=3.6',
 )
